bf9dd4c729f8b808eecd43f9356442375cedc58d
Juan Cuartas <kuartas77@gmail.com>
2023-06-27 15:12:48 -0500

    update package.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,8 @@
 const mix = require('laravel-mix');
-
+const path = require('path');
+mix.alias({
+    '@': path.join(__dirname, 'resources/js')
+});
 mix.js('resources/js/app.js', 'public/js').vue()
 //  .postCss('resources/css/app.css', 'public/css', [
 //      //
