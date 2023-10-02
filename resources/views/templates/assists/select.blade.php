6e198c9c08738d8d221139c5a81231160f3670b6
Juan Cuartas <kuartas77@gmail.com>
2023-10-02 10:15:15 -0500

    fix assist colors

--- resources/views/templates/assists/select.blade.php
+++ resources/views/templates/assists/select.blade.php
@@ -1,18 +1,18 @@
 @switch($value)
     @case('as')
-        {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist form-success', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
+        {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist color-success', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
     @break
     @case('fa')
-        {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist form-error', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
+        {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist color-error', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
     @break
     @case('ex')
-        {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist form-orange', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
+        {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist color-orange', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
     @break
     @case('re')
-        {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist form-grey', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
+        {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist color-grey', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
     @break
     @case('in')
-        {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist form-warning', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
+        {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist color-warning', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
     @break
     @default
         {!! Form::select($column, $optionAssist, $value,  ['class' => 'form-control form-control-sm assist', 'placeholder'=>'Selecciona...', $deleted ? 'disabled' : '' ]) !!}
