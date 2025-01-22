36138486a3bf22356492cccacff38c8fff9c9318
Juan Cuartas <kuartas77@gmail.com>
2025-01-21 22:53:46 -0500

    add resources contract

--- resources/views/templates/contracts/10pro/header.blade.php
+++ resources/views/templates/contracts/10pro/header.blade.php
@@ -7,7 +7,7 @@
             <td class="text-center school-title" width="60%">
                 <p><strong>Formato</strong></p>
                 <br>
-                <p><strong>{{ strtoupper($school->name) }}</strong></p>
+                <p><strong>Inscripción Academia de Fútbol</strong></p>
             </td>
             <td class="text-center" width="20%">
                 <table class="table-full detail detail-lines">
