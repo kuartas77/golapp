093534b7376c43a96e599a43922f2e3de2e175b6
Juan Cuartas <kuartas77@gmail.com>
2025-09-27 15:10:27 -0500

    player list updated

--- resources/js/pages/players/PlayersList.vue
+++ resources/js/pages/players/PlayersList.vue
@@ -1,25 +1,7 @@
 <template>
     <panel>
         <template #body>
-            <DatatableTemplate :options="options" :id="'players_table'" ref="table" @click="resolveRouteFromClick($event)">
-
-
-
-                <template #link="props">
-                    <div class="text-center">
-                        <a href="#" :data-item-id="props.cellData" class="link-primary">
-                            {{ props.rowData.unique_code}}
-                        </a>
-                    </div>
-                </template>
-
-                <template #date="props">
-                    <div class="text-center">
-                        {{ dayjs(props.cellData).format('l') }}
-                    </div>
-                </template>
-
-            </DatatableTemplate>
+            <DatatableTemplate :options="options" :id="'players_table'" ref="table" @click="resolveRouteFromClick($event)"/>
         </template>
     </panel>
 
