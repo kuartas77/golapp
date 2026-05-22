a5a8bdcdd44d91b150850cfdbbfc8c2680ccbe78
Juan Cuartas <kuartas77@gmail.com>
2026-05-22 09:01:32 -0500

    feat(reports): consolidate debtor PDF rows

--- resources/views/templates/pdf/debtors.blade.php
+++ resources/views/templates/pdf/debtors.blade.php
@@ -29,8 +29,7 @@
             <th class="text-center">Código</th>
             <th>Deportista</th>
             <th>Categoría</th>
-            <th>Mensualidades</th>
-            <th>Item</th>
+            <th>Debe</th>
             @if($showTotalDebt)
                 <th class="text-right">Total Deuda</th>
             @endif
@@ -43,20 +42,23 @@
                 <td class="text-center">&nbsp;{{ $row['unique_code'] }}&nbsp;</td>
                 <td>&nbsp;{{ $row['student_name'] }}&nbsp;</td>
                 <td>&nbsp;{{ $row['category'] }}&nbsp;</td>
-                <td>&nbsp;{{ $row['monthly_debt_label'] ? 'Debe: '.$row['monthly_debt_label'] : '--' }}&nbsp;</td>
-                <td>&nbsp;{{ $row['item_debt_label'] ?: '--' }}&nbsp;</td>
+                <td>
+                    @foreach($row['debt_items'] as $debtItem)
+                        <div>&nbsp;{{ $debtItem['label'] }}&nbsp;</div>
+                    @endforeach
+                </td>
                 @if($showTotalDebt)
                     <td class="text-right">&nbsp;{{ number_format($row['total_debt'], 0, ',', '.') }}&nbsp;</td>
                 @endif
             </tr>
         @empty
             <tr>
-                <td colspan="{{ $showTotalDebt ? 7 : 6 }}" class="text-center">&nbsp;No se encontraron deudores para los filtros seleccionados.&nbsp;</td>
+                <td colspan="{{ $showTotalDebt ? 6 : 5 }}" class="text-center">&nbsp;No se encontraron deudores para los filtros seleccionados.&nbsp;</td>
             </tr>
         @endforelse
         @if($showTotalDebt)
             <tr>
-                <th colspan="6" class="text-right">Total deuda:</th>
+                <th colspan="5" class="text-right">Total deuda:</th>
                 <th class="text-right">&nbsp;{{ number_format($rows->sum('total_debt'), 0, ',', '.') }}&nbsp;</th>
             </tr>
         @endif
